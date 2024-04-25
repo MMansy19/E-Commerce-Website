3f402ebcd8aa11ed7bd0486934b3c74b538d7ad7
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-04-25 14:10:59 +0200

    Update Home.jsx

--- E-Commerce/src/Pages/Home.jsx
+++ E-Commerce/src/Pages/Home.jsx
@@ -5,7 +5,7 @@ import Categories from "../components/Home/Categories";
 import Services from "../components/common/Services";
 const ITEMS = [
   {
-    imageSrc: "./public/assets/car.svg",
+    imageSrc: "./assets/car.svg",
     title: "Kids Electric Car",
     price: 960,
     stars: Math.floor(Math.random() * 5),
@@ -13,7 +13,7 @@ const ITEMS = [
     discount: "",
   },
   {
-    imageSrc: "./public/assets/camera.svg",
+    imageSrc: "./assets/camera.svg",
     title: "CANON EOS DSLR Camera",
     price: 360,
     stars: Math.floor(Math.random() * 5),
@@ -21,7 +21,7 @@ const ITEMS = [
     discount: "",
   },
   {
-    imageSrc: "./public/assets/dogfood.svg",
+    imageSrc: "./assets/dogfood.svg",
     title: "Breed Dry Dog Food",
     price: 100,
     stars: Math.floor(Math.random() * 5),
@@ -29,7 +29,7 @@ const ITEMS = [
     discount: "",
   },
   {
-    imageSrc: "./public/assets/labtop.svg",
+    imageSrc: "./assets/labtop.svg",
     title: "ASUS FHD Gaming Laptop",
     price: 700,
     stars: Math.floor(Math.random() * 5),
@@ -37,7 +37,7 @@ const ITEMS = [
     discount: "",
   },
   {
-    imageSrc: "./public/assets/cream.svg",
+    imageSrc: "./assets/cream.svg",
     title: "Curology Product Set",
     price: 500,
     stars: Math.floor(Math.random() * 5),
@@ -45,7 +45,7 @@ const ITEMS = [
     discount: "",
   },
   {
-    imageSrc: "./public/assets/g-black.svg",
+    imageSrc: "./assets/g-black.svg",
     title: "GP11 Shooter USB Gamepad",
     price: 660,
     stars: Math.floor(Math.random() * 5),
@@ -53,7 +53,7 @@ const ITEMS = [
     discount: 40,
   },
   {
-    imageSrc: "./public/assets/jacket.svg",
+    imageSrc: "./assets/jacket.svg",
     title: "Quilted Satin Jacket",
     price: 660,
     stars: Math.floor(Math.random() * 5),
@@ -61,7 +61,7 @@ const ITEMS = [
     discount: 40,
   },
   {
-    imageSrc: "./public/assets/bookself.svg",
+    imageSrc: "./assets/bookself.svg",
     title: "Small BookSelf",
     price: 360,
     stars: Math.floor(Math.random() * 5),
@@ -69,7 +69,7 @@ const ITEMS = [
     discount: 40,
   },
   {
-    imageSrc: "./public/assets/headphones.svg",
+    imageSrc: "./assets/headphones.svg",
     title: "RGB liquid CPU Cooler",
     price: 160,
     stars: Math.floor(Math.random() * 5),
@@ -77,7 +77,7 @@ const ITEMS = [
     discount: 40,
   },
   {
-    imageSrc: "./public/assets/bag.svg",
+    imageSrc: "./assets/bag.svg",
     title: "Gucci duffle bag",
     price: 1160,
     stars: Math.floor(Math.random() * 5),
@@ -85,7 +85,7 @@ const ITEMS = [
     discount: 40,
   },
   {
-    imageSrc: "./public/assets/coat.svg",
+    imageSrc: "./assets/coat.svg",
     title: "The north coat",
     price: 360,
     stars: Math.floor(Math.random() * 5),
@@ -93,7 +93,7 @@ const ITEMS = [
     discount: 40,
   },
   {
-    imageSrc: "./public/assets/g-colored.svg",
+    imageSrc: "./assets/g-colored.svg",
     title: "HAVIT HV-G92 Gamepad",
     price: 160,
     stars: Math.floor(Math.random() * 5),
@@ -101,7 +101,7 @@ const ITEMS = [
     discount: 40,
   },
   {
-    imageSrc: "./public/assets/keyboard.svg",
+    imageSrc: "./assets/keyboard.svg",
     title: "AK-900 Wired Keyboard",
     price: 1160,
     stars: Math.floor(Math.random() * 5),
@@ -109,7 +109,7 @@ const ITEMS = [
     discount: 35,
   },
   {
-    imageSrc: "./public/assets/tv.svg",
+    imageSrc: "./assets/tv.svg",
     title: "IPS LCD Gaming Monitor",
     price: 400,
     stars: Math.floor(Math.random() * 5),
@@ -117,7 +117,7 @@ const ITEMS = [
     discount: 30,
   },
   {
-    imageSrc: "./public/assets/chair.svg",
+    imageSrc: "./assets/chair.svg",
     title: "S-Series Comfort Chair ",
     price: 400,
     stars: Math.floor(Math.random() * 5),
